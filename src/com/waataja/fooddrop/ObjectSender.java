04332bd25c6dab97d66d2fef802ad5a7101f4f7b
laelath <jf.laelath@gmail.com>
2016-10-14 14:20:19 -0700

    Added server and client networking code

---
+++ src/com/waataja/fooddrop/ObjectSender.java
@@ -0,0 +1,75 @@
+package com.waataja.fooddrop;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.List;
+import java.util.ArrayList;
+
+import com.waataja.fooddrop.FoodMessage.MessageType;
+
+public class ObjectSender {
+	
+	@SuppressWarnings("unchecked")
+	public static List<Giveaway> getGiveaways(FoodReceiver reciever) {
+		try {
+			Socket sock = new Socket("psv3.org", 8792);
+			try {
+				ObjectOutputStream writer = new ObjectOutputStream(sock.getOutputStream());
+				writer.writeObject(reciever);
+				writer.flush();
+				writer.close();
+				ObjectInputStream reader = new ObjectInputStream(sock.getInputStream());
+				FoodMessage msg = (FoodMessage) reader.readObject();
+				List<Giveaway> giveaways = null;
+				if (msg.getType() == MessageType.RETURN) {
+					giveaways = (ArrayList<Giveaway>) msg.getObject();
+				} else {
+					System.out.println("Illegal message type");
+				}
+				reader.close();
+				sock.close();
+				return giveaways;
+			} catch (IOException e) {
+				sock.close();
+				System.out.println("Connection IO error");
+				e.printStackTrace();
+				return null;
+			} catch (ClassNotFoundException e) {
+				sock.close();
+				System.out.println("Connection message error");
+				e.printStackTrace();
+				return null;
+			}
+		} catch (IOException e) {
+			System.out.println("Error establishing connection");
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public static boolean addGiveaway(Giveaway giveaway) {
+		try {
+			Socket sock = new Socket("psv3.org", 8792);
+			try {
+				ObjectOutputStream writer = new ObjectOutputStream(sock.getOutputStream());
+				writer.writeObject(giveaway);
+				writer.flush();
+				writer.close();
+				sock.close();
+				return true;
+			} catch (IOException e) {
+				sock.close();
+				System.out.println("Connection IO error");
+				e.printStackTrace();
+				return false;
+			}
+		} catch (IOException e) {
+			System.out.println("Error establishing connection");
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+}
---
+++ src/com/waataja/fooddrop/server/FoodDropServer.java
@@ -0,0 +1,72 @@
+package com.waataja.fooddrop.server;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
+import com.waataja.fooddrop.*;
+import com.waataja.fooddrop.FoodMessage.MessageType;
+
+public class FoodDropServer {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		ServerSocket servSock = null;
+		try {
+			servSock = new ServerSocket(8729, 5);
+		} catch (IOException e) {
+			System.out.println("Unable to create socket");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		try {
+			servSock.setReuseAddress(true);
+			servSock.setSoTimeout(10000);
+		} catch (SocketException e1) {
+			System.out.println("Unable to set socket properties.");
+			e1.printStackTrace();
+			System.exit(1);;
+		}
+		while (true) {
+			try {
+				Socket sock = servSock.accept();
+				try {
+					ObjectInputStream reader = new ObjectInputStream(sock.getInputStream());
+					FoodMessage msg = (FoodMessage) reader.readObject();
+					reader.close();
+					if (msg.getType() == MessageType.REQUEST) {
+						FoodReceiver receiver = (FoodReceiver) msg.getObject();
+						//Use reciever to get a list of giveaways
+						//FoodMessage giveList = new FoodMessage(MessageType.RETURN, );
+						//ObjectOutputStream writer = new ObjectOutputStream(sock.getOutputStream());
+						//writer.writeObject(giveList);
+						//writer.flush();
+						//writer.close();
+					} else if (msg.getType() == MessageType.ADD) {
+						Giveaway giveaway = (Giveaway) msg.getObject();
+						//Add to database here
+					} else {
+						System.out.println("Illegal message type");
+					}
+					sock.close();
+				} catch (IOException e) {
+					sock.close();
+					System.out.println("Connection IO error");
+					e.printStackTrace();
+					continue;
+				} catch (ClassNotFoundException e) {
+					sock.close();
+					System.out.println("Connection message error");
+					e.printStackTrace();
+				}
+			} catch (IOException e) {
+				System.out.println("Error accepting connection");
+				e.printStackTrace();
+				continue;
+			}
+		}
+	}
+
+}
